4e90144d6a3b6d9fee5fa6baaa42d906bbe25897
Apple <apple@Apples-MBP.bbrouter>
2020-12-09 22:08:24 +0530

    Added test cases

---
+++ SampleCocoaPods/SampleCocoaPods.swift
@@ -0,0 +1,22 @@
+//
+//  SampleCocoaPods.swift
+//  SampleCocoaPods
+//
+//  Created by Apple on 09/12/20.
+//
+
+import Foundation
+
+public final class SampleCocoaPods {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SampleCocoaPodsTests/SampleCocoaPodsTests.swift
+++ SampleCocoaPodsTests/SampleCocoaPodsTests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 class SampleCocoaPodsTests: XCTestCase {
 
+    var swiftyLib: SampleCocoaPods!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,11 +19,19 @@ class SampleCocoaPodsTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    override func setUp() {
+        swiftyLib = SampleCocoaPods()
+    }
 
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testAdd() {
+           XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+       }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
